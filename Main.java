8a67aa82e4eec6b07c993d3a2ba3d78dc9acdcb9
rizry <rizzwan.ansarii@gmail.com>
2022-10-04 18:55:18 +0530

    checked all email samples validation

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 package com.java.day19;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -12,37 +13,73 @@ public class Main {
     checkEmail();
     checkPhoneNumber();
     checkPassword();
+    testValidEmails();
 
   }
 
   public static void checkFirstName() {
     System.out.print("Enter first name: ");
     String fName = SC.next();
-    UserDetailsValidation.isValidName(fName);
+    System.out.println(UserDetailsValidation.isValidName(fName));
   }
 
   public static void checkLastName() {
     System.out.print("Enter last name: ");
     String lName = SC.next();
-    UserDetailsValidation.isValidName(lName);
+    System.out.println(UserDetailsValidation.isValidName(lName));
   }
 
   public static void checkEmail() {
     System.out.print("Enter an email: ");
     String email = SC.next();
-    UserDetailsValidation.isValidEmail(email);
+    System.out.println(UserDetailsValidation.isValidEmail(email));
   }
 
   public static void checkPhoneNumber() {
     System.out.print("Enter a phone number: ");
     SC.nextLine();
     String phNum = SC.nextLine();
-    UserDetailsValidation.isValidPhoneNumber(phNum);
+    System.out.println(UserDetailsValidation.isValidPhoneNumber(phNum));
   }
 
   public static void checkPassword() {
     System.out.print("Enter password: ");
     String password = SC.next();
-    UserDetailsValidation.isValidPassword(password);
+    System.out.println(UserDetailsValidation.isValidPassword(password));
   }
+
+  public static void testValidEmails() {
+    ArrayList<String> emails = new ArrayList<>();
+    // Valid emails
+    emails.add("abc@yahoo.com");
+    emails.add("abc-100@yahoo.com");
+    emails.add("abc.100@yahoo.com");
+    emails.add("abc111@abc.com");
+    emails.add("abc-100@abc.net");
+    emails.add("abc.100@abc.com.au");
+    emails.add("abc@1.com");
+    emails.add("abc@gmail.com.com");
+    emails.add("abc+100@gmail.com");
+
+    // Invalid emails
+    emails.add("\n");
+    emails.add("abc");
+    emails.add("abc@.com.my");
+    emails.add("abc123@gmail.a");
+    emails.add("abc123@.com");
+    emails.add("abc123@.com.com");
+    emails.add(".abc@abc.com");
+    emails.add("abc()*@gmail.com");
+    emails.add("abc@%*.com");
+    emails.add("abc..2002@gmail.com");
+    emails.add("abc.@gmail.com");
+    emails.add("abc@abc@gmail.com");
+    emails.add("abc@gmail.com.1a");
+    emails.add("abc@gmail.com.aa.au");
+
+    for (String email : emails) {
+      System.out.println(email + " : " + UserDetailsValidation.isValidEmail(email));
+    }
+  }
+
 }
--- UserDetailsValidation.java
+++ UserDetailsValidation.java
@@ -7,28 +7,28 @@ public class UserDetailsValidation {
 
   static String regex;
 
-  public static void isValidName(String name) {
+  public static String isValidName(String name) {
     regex = "^[A-Z]{1}[a-z]{2,}$";
-    if (validate(regex, name)) System.out.println("valid");
-    else System.out.println("invalid");
+    if (validate(regex, name)) return "valid";
+    else return "invalid";
   }
 
-  public static void isValidEmail(String email) {
-    regex = "^(\\w(.\\w)?){3,}[@][a-z]{2,5}[.][a-z]{2,3}(.[a-z]{2,3})?";
-    if (validate(regex, email)) System.out.println("valid");
-    else System.out.println("invalid");
+  public static String isValidEmail(String email) {
+    regex = "^[a-zA-Z]+[\\w]*([.|\\-|+][a-zA-Z0-9]+)*@[\\w]{1,5}[.][a-z]{2,3}(.[a-z]{2,3})?$";
+    if (validate(regex, email)) return "valid";
+    else return "invalid";
   }
 
-  public static void isValidPhoneNumber(String phNum) {
+  public static String isValidPhoneNumber(String phNum) {
     regex = "^[0-9]{1,3}(-[0-9]{1,3})?\\s{0,3}[6-9]{1}[0-9]{9}$";
-    if (validate(regex, phNum)) System.out.println("valid");
-    else System.out.println("invalid");
+    if (validate(regex, phNum)) return "valid";
+    else return "invalid";
   }
 
-  public static void isValidPassword(String pass) {
-    regex = "^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,}$";
-    if (validate(regex, pass)) System.out.println("valid");
-    else System.out.println("invalid");
+  public static String isValidPassword(String pass) {
+    regex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,}$";
+    if (validate(regex, pass)) return "valid";
+    else return "invalid";
   }
 
   private static boolean validate(String regex, String pattern) {
